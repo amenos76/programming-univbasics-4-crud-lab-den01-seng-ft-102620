6539f3880e2c592a2fec54295080325ea50a52ce
Augie Menos <augie.menos@gmail.com>
2020-10-14 20:09:34 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -27,7 +27,7 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
-  array[0]
+  array.shift(array[0])
 end
 
 def retrieve_last_element_from_array(array)
@@ -35,5 +35,5 @@ def retrieve_last_element_from_array(array)
 end
 
 def update_element_from_index(array, index_number, element)
-  element = array[index_number]
+  array.shift()
 end
